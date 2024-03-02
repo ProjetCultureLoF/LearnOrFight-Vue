311e243b72a3423db6b37c55eb3d6d2d2e72ddd1
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-03-02 15:20:10 +0100

    Texte de victoire

--- vue-project/src/views/Departments.vue
+++ vue-project/src/views/Departments.vue
@@ -1,6 +1,10 @@
 <template>
-  {{ $route.params.dep }}
-  <div v-if="quizes.length > 0">
+  <div v-if="isFinished">
+    <h1>Vous avez terminé le quiz !</h1>
+    <h2>{{ goodAnswer }}</h2>
+    <h2>{{ badAnswer }}</h2>
+  </div>
+  <div v-else-if="quizes.length > 0">
     <Quiz :quiz="quizes[currentQuiz]" @nextQuestion="nextQuestion" />
   </div>
 </template>
@@ -18,6 +22,7 @@ const currentQuiz = ref(0);
 
 const goodAnswer = ref(0);
 const badAnswer = ref(0);
+const isFinished = ref(false);
 
 async function getQuizes() {
   const departments = await api.get(
@@ -38,13 +43,24 @@ async function getQuizes() {
 }
 
 function nextQuestion(answer) {
-  if (currentQuiz.value + 1 < quizes.value.length) {
+  if (currentQuiz.value < quizes.value.length) {
     currentQuiz.value++;
-  }
-  if (answer) {
-    goodAnswer.value += 1;
-  } else {
-    badAnswer.value += 1;
+    if (answer) {
+      goodAnswer.value += 1;
+    } else {
+      badAnswer.value += 1;
+    }
+    console.log("Comp:", currentQuiz.value + 1 == quizes.value.length);
+    console.log(
+      "CurrentQuizz: ",
+      currentQuiz.value,
+      "Quiz L: ",
+      quizes.value.length
+    );
+    if (currentQuiz.value == quizes.value.length) {
+      isFinished.value = true;
+    }
+    console.log(isFinished.value);
   }
 }
 
