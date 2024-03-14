e67ca00482bbf51073d92f36f4caec184f5a7e31
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-03-14 16:49:42 +0100

    Ajout d'une route account

--- vue-project/src/router.js
+++ vue-project/src/router.js
@@ -1,42 +1,42 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '@/views/home.vue';
-import Departments from '@/views/Departments.vue';
-import NotFound from '@/views/pageNotFound.vue';
-import Admin from '@/views/AdminQuizz.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/home.vue";
+import Departments from "@/views/Departments.vue";
+import NotFound from "@/views/pageNotFound.vue";
+import Admin from "@/views/AdminQuizz.vue";
 
-// import Account from './views/Account.vue';
+import Account from "./views/Account.vue";
 // import Leaderboard from './components/Leaderboard.vue';
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home,
     },
 
     {
-      path: '/departments/:dep',
+      path: "/departments/:dep",
       name: "departments",
-      component: Departments
+      component: Departments,
     },
     {
-      path: '/admin',
+      path: "/admin",
       name: "admin",
-      component: Admin
+      component: Admin,
     },
-    // {
-    //   path: '/account',
-    //   component: Account
-    // },
+    {
+      path: "/account",
+      component: Account,
+    },
     // {
     //   path: '/leaderboard',
     //   component: Leaderboard
     // }
     {
-      path:"/:catchall(.*)*",
-      component: NotFound
+      path: "/:catchall(.*)*",
+      component: NotFound,
     },
-  ]
-})
+  ],
+});
