c9643fd7785735893f3177dacdf87d3bb18b2a9d
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-03-19 14:46:54 +0100

    Verification mdp

---
+++ vue-project/src/components/Verify.vue
@@ -0,0 +1,61 @@
+<template>
+  <Overlay />
+  <transition name="fade">
+    <div
+      class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-3 pb-8 shadow-lg rounded-lg w-2/6 min-w-max h-fill flex flex-col text-center items-center z-50 gap-7"
+    >
+      <img
+        src="/src/assets/icons/clair.png"
+        class="self-end w-8 hover:w-10 duration-200"
+        @click="$emit('close')"
+      />
+      <h2>Validez votre mot de passe</h2>
+      <form @submit.prevent="login" class="flex flex-col gap-5">
+        <div class="flex flex-col gap-5">
+          <input
+            id="password"
+            v-model="password"
+            type="password"
+            required
+            placeholder="Mot de passe"
+          />
+        </div>
+        <p v-if="errorMessage" class="text-red-600">{{ errorMessage }}</p>
+        <button class="bg-[#a76a30]" @click="verifyUser" type="submit">
+          Se connecter
+        </button>
+      </form>
+    </div>
+  </transition>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Overlay from "@/components/Overlay.vue";
+import CryptoJS from "crypto-js";
+import { errorMessages } from "vue/compiler-sfc";
+
+const props = defineProps({
+  userData: Object,
+});
+
+const emit = defineEmits(["validate", "close"]);
+
+const password = ref("");
+const errorMessage = ref("");
+
+async function verifyUser() {
+  const salt = "your-salt-string";
+  const hashedPassword = CryptoJS.PBKDF2(password.value, salt, {
+    keySize: 512 / 32,
+    iterations: 1000,
+  }).toString();
+
+  if (hashedPassword == props.userData.Password_User) {
+    emit("validate");
+    errorMessage.value = "";
+  } else {
+    errorMessage.value = "Mot de passe incorrect";
+  }
+}
+</script>
