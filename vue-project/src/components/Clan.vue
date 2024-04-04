6e2e7ba1d8b1b82b3a0acd94329e85e36fe868ea
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-04-04 11:30:52 +0200

    Modification affichage clans

--- vue-project/src/components/Clan.vue
+++ vue-project/src/components/Clan.vue
@@ -1,18 +1,59 @@
 <template>
-  <div id="clan" style="background-color: green">
-    <h1>{{ clan.name }}</h1>
+  <Verify
+    v-if="showOverlay"
+    @validate="changeUserClan"
+    @close="showOverlay = false"
+    :warning="'Attention, changer de clan effacera tous vos scores'"
+  />
+  <div
+    id="clan"
+    class="flex flex-col flex-wrap w-1/3 items-center justify-center gap-4"
+  >
+    <h1>{{ clan.Title_Clan }}</h1>
     <img
-      :src="`src/assets/logo/${clan.name}.png`"
+      :src="`src/assets/logo/${clan.Title_Clan}.png`"
       alt=""
-      style="height: 150px"
+      class="h-[15vh]"
     />
-    <p>{{ clan.players }}</p>
-    <p>{{ clan.score }}</p>
+    <button class="p-2" @click="showOverlay = true">Changer de clan</button>
+    <ScoreBoard
+      :title="'Meilleurs joueurs'"
+      :headers="headers"
+      :listScore="userScore"
+    />
   </div>
 </template>
 
 <script setup>
+import ScoreBoard from "./ScoreBoard.vue";
+import Verify from "./Verify.vue";
+import { ref, onMounted } from "vue";
+import { api } from "@/plugins/requete";
+
+const userScore = ref();
+const showOverlay = ref(false);
+const headers = ref([
+  { key: "index", label: "N°" },
+  { key: "Name_User", label: "Pseudo" },
+  { key: "maxScore", label: "Total points" },
+]);
+
 const props = defineProps({
   clan: Object,
+});
+
+async function changeUserClan(ID_User) {
+  const response = await api.patch(
+    `/users/${ID_User}/?clanIDClan=${props.clan.ID_Clan}`
+  );
+  console.log(response.data);
+  showOverlay.value = false;
+  window.location.reload();
+}
+
+onMounted(async () => {
+  const response = await api.get(`/scores/?clanIDClan=${props.clan.ID_Clan}`);
+  userScore.value = response.data;
+  console.log(userScore.value);
 });
 </script>
