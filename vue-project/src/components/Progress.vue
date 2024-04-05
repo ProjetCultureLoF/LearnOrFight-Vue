b388b4feb354f8a683b0c10d4ddc2afd03af8960
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-04-05 13:26:17 +0200

    Changement avancé du quiz
    Envoie d'une liste de boolean au lieu d'un boolean et style bounce

--- vue-project/src/components/Progress.vue
+++ vue-project/src/components/Progress.vue
@@ -4,42 +4,31 @@
       <div
         class="flex flex-row border-black border-4 rounded-full w-6 h-6 justify-center duration-200"
         :class="{
-          '': index == current,
-          'bg-blue-500': index <= current && listWasTrue[index],
+          'animate-bounce  ': index == current,
+          '!bg-blue-500': index <= current && listWasTrue[index],
 
-          'bg-red-500': listWasTrue[index] === false || index > current,
+          '!bg-red-500': listWasTrue[index] === false || index > current,
         }"
         v-for="(element, index) of list"
         :key="index"
       >
-        <div class="flex flex-col relative top-full bg-transparent">
-          <p v-if="index <= current" class="xs:text-base font-semibold">
-            {{ element.Question_Quiz }}
-          </p>
-          <p v-else class="flex flex-col relative top-full italic">?</p>
-        </div>
+        <div class="flex flex-col relative top-full bg-transparent"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, watch, computed } from "vue";
+import { ref, watch, inject } from "vue";
 const props = defineProps({
   list: Array,
   current: Number,
-  wasTrue: Boolean,
+  listWasTrue: Array,
 });
 
-const listWasTrue = ref([]);
-
 watch(
-  () => props.wasTrue,
-  (newValue) => {
-    if (newValue != null) {
-      listWasTrue.value.push(newValue);
-    }
-    console.log(listWasTrue.value);
-  }
+  () => props.current,
+  (newValue) => {}
 );
+console.log("Liste true/false: ", props.listWasTrue);
 </script>
