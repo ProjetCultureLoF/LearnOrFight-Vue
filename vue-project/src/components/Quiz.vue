291bf9b51054dadf4244470028b8d6d48af2d6d7
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-04-11 09:41:28 +0200

    Possibilité modification question quizz

--- vue-project/src/components/Quiz.vue
+++ vue-project/src/components/Quiz.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="answers" class="flex flex-col items-center text-center w-1/2 gap-5">
-    <h1 class="">{{ quiz.Question_Quiz }}</h1>
+    <h1 v-on:dblclick="changeTitle" v-if="isTitle == false" class="">
+      {{ quiz.Question_Quiz }}
+    </h1>
+    <div v-if="isTitle == true" class="flex gap-4">
+      <textarea
+        id="w3review"
+        name="w3review"
+        rows="4"
+        cols="50"
+        class="bg-gray-100 p-1 w-[50vw] h-[5vh]"
+        v-model="newTitle"
+        v-on:dblclick="changeTitle"
+      ></textarea>
+      <button @click="uploadNewTitle">Changer</button>
+    </div>
     <h3 class="">{{ quiz.themes[0].Title_Theme }}</h3>
     <h2 v-if="admin == false" class="duration-200">{{ time }}</h2>
 
@@ -10,8 +24,8 @@
         v-for="(answer, index) in quiz.answers"
         :key="index"
         :answer="answer"
+        :answering="answering"
         :selectedAnswer="selectedAnswer"
-        :goodAnswer="goodAnswer"
         @selectAnswer="selectAnswer"
       >
       </Answers>
@@ -36,7 +50,7 @@
   </div>
   <button
     v-if="admin"
-    class="duration-200 rounded-md p-4 text-xs lg:text-2xl shadow"
+    class="duration-200 rounded-md p-2 w-fit"
     @click="$emit('modify', selectedAnswer, quiz, goodAnswer)"
   >
     Changer réponse
@@ -71,6 +85,9 @@ const errorMessages = ref("");
 const time = props.admin ? ref(0) : ref(30);
 const timer = ref(null);
 const goodAnswer = ref(null);
+const isTitle = ref(false);
+const newTitle = ref(props.quiz.Question_Quiz);
+const answering = ref(false);
 
 function selectAnswer(answer) {
   console.log(answer);
@@ -78,19 +95,39 @@ function selectAnswer(answer) {
   selectedAnswer.value = answer;
 }
 
-async function isAnswer(answer) {
-  if (answer != null) {
-    const response = await api.get(
-      `/quizAnswers/?quizIDQuiz=${props.quiz.ID_Quiz}&Is_QuizAnswer=1`
-    );
+function changeTitle() {
+  if (props.admin === true) {
+    isTitle.value = !isTitle.value;
+  }
+}
 
-    if (answer.ID_Answer == response.data[0].answer.ID_Answer) {
+async function uploadNewTitle() {
+  try {
+    await api
+      .patch(
+        `/quiz/${props.quiz.ID_Quiz}?Question_Quiz=${encodeURIComponent(
+          newTitle.value
+        )}`
+      )
+      .then((response) => {
+        props.quiz.Question_Quiz = newTitle.value;
+        isTitle.value = false;
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function isAnswer(answer) {
+  console.log(answer);
+  if (answer != null && answer.ID_Answer != 0) {
+    answering.value = false;
+
+    if (answer.quizAnswer.Is_QuizAnswer) {
       emit("validate", true);
     } else {
       emit("validate", false);
     }
-
-    goodAnswer.value = response.data[0].answer;
 
     errorMessages.value = "";
   } else {
@@ -100,7 +137,6 @@ async function isAnswer(answer) {
 
 function nextQuestion() {
   selectedAnswer.value = null;
-  goodAnswer.value = null;
   emit("nextQuestion");
 }
 
@@ -112,14 +148,17 @@ watch(
       props.waitNext == false &&
       route.name.includes("departments")
     ) {
+      answering.value = true;
       setTimeout(() => {
         time.value--;
       }, 1000);
     }
     if (count == 0 && selectedAnswer.value == null) {
       isAnswer({ ID_Answer: 0 });
+      answering.value = false;
     } else if (count == 0) {
       isAnswer(selectedAnswer.value);
+      answering.value = false;
     }
   },
   {
@@ -132,6 +171,7 @@ watch(
   (newWaitNext) => {
     if (newWaitNext == false) {
       time.value = 30;
+      answering.value = true;
     }
   }
 );
