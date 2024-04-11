e2eaf951b748693b6232a4b67c692481b576b883
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-04-11 09:40:52 +0200

    Affichage

--- vue-project/src/components/Overlay.vue
+++ vue-project/src/components/Overlay.vue
@@ -1,6 +1,6 @@
 <template>
   <div
     id="overlay"
-    class="flex w-full h-full bg-opacity-70 bg-black absolute top-0 z-10 items-center justify-center content-center"
+    class="flex w-full h-full bg-opacity-70 bg-black absolute top-0 left-0 z-10 items-center justify-center content-center self-center"
   ></div>
 </template>
