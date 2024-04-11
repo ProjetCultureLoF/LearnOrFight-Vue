2a9f9abdb1e175b5e96c946144f4e777a7f8ed03
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-04-11 09:39:50 +0200

    Modification affichage

--- vue-project/src/components/Answers.vue
+++ vue-project/src/components/Answers.vue
@@ -2,23 +2,33 @@
   <div
     @click="selectAnswer(answer)"
     :class="{
-      'bg-[#ffffff]': goodAnswer == null || goodAnswer.ID_Answer == undefined,
+      // 'bg-[#ffffff]': goodAnswer == null || goodAnswer.ID_Answer == undefined,
+      'bg-[#ffffff]': answering == true,
       'bg-[#ed4e4e] border-[#d63d3d] duration-200':
-        goodAnswer !== null &&
-        goodAnswer.ID_Answer !== undefined &&
-        goodAnswer.ID_Answer !== answer.ID_Answer,
+        // goodAnswer !== null &&
+        // goodAnswer.ID_Answer !== undefined &&
+        // goodAnswer.ID_Answer !== answer.ID_Answer,
+        answer.quizAnswer.Is_QuizAnswer == false && answering == false,
       'bg-[#614ddf] border-[#5340cd] duration-200':
-        goodAnswer !== null && goodAnswer.ID_Answer === answer.ID_Answer,
+        // goodAnswer !== null && goodAnswer.ID_Answer === answer.ID_Answer,
+        answer.quizAnswer.Is_QuizAnswer == true && answering == false,
       'shadow-red-600 shadow-lg':
+        // answer == selectedAnswer &&
+        // goodAnswer !== null &&
+        // goodAnswer.ID_Answer !== answer.ID_Answer,
         answer == selectedAnswer &&
-        goodAnswer !== null &&
-        goodAnswer.ID_Answer !== answer.ID_Answer,
+        answer.quizAnswer.Is_QuizAnswer == false &&
+        answering == false,
+
       'shadow-[#614ddf] shadow-lg':
+        // answer == selectedAnswer &&
+        // goodAnswer !== null &&
+        // goodAnswer.ID_Answer === answer.ID_Answer,
         answer == selectedAnswer &&
-        goodAnswer !== null &&
-        goodAnswer.ID_Answer === answer.ID_Answer,
+        answer.quizAnswer.Is_QuizAnswer == true &&
+        answering == false,
     }"
-    class="w-[20vw] lg:w-[23vw] border-t border-l border-r-4 border-b-4 rounded-md shadow duration-200 p-1 lg:p-2 hover:shadow-lg active:w-[24vw] md:active:w-[22vw]"
+    class="w-[20vw] lg:w-[23vw] xs:w-[21vw] border-t border-l border-r-4 border-b-4 rounded-md shadow duration-200 p-1 lg:p-2 hover:shadow-lg active:w-[24vw] md:active:w-[22vw] xs:active:-[21vw ]"
   >
     <p
       :class="{ 'text-[#f4b44e]': selectedAnswer === answer }"
@@ -34,10 +44,7 @@
 const props = defineProps({
   answer: Object,
   selectedAnswer: Object,
-  goodAnswer: {
-    type: Object,
-    default: null,
-  },
+  answering: Boolean,
 });
 
 const emit = defineEmits(["selectAnswer"]);
@@ -48,6 +55,8 @@ function selectAnswer(answer) {
   }
   emit("selectAnswer", answer);
 }
+
+console.log(props.answer.quizAnswer.Is_QuizAnswer);
 </script>
 
 <style scoped>
