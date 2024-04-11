02a20782b85fed556e87d266357c634ba9908135
Azene <elie.lajoinie@supdevinci-edu.fr>
2024-04-11 09:42:27 +0200

    Update requete.js

--- vue-project/src/plugins/requete.js
+++ vue-project/src/plugins/requete.js
@@ -1,38 +1,38 @@
-import axios from 'axios';
+import axios from "axios";
+import { ip } from "@/plugins/ip.js";
 
 class API {
-    constructor() {
-        this.axios = axios.create({
-            baseURL: 'http://localhost:3001/api/v1/',
+  constructor() {
+    this.axios = axios.create({
+      baseURL: "http://" + ip + ":3001/api/v1/",
+    });
+  }
 
-        });
-    }
+  async get(url) {
+    return await this.axios.get(url);
+  }
 
-   async get(url) {
-        return await this.axios.get(url);
-    }
+  async post(url, data) {
+    return await this.axios.post(url, data);
+  }
 
-    async post(url, data) {
-        return await this.axios.post(url, data);
-    }
+  async put(url, data) {
+    return await this.axios.put(url, data);
+  }
 
-    async put(url, data) {
-        return await this.axios.put(url, data);
-    }
+  async delete(url) {
+    return await this.axios.delete(url);
+  }
 
-    async delete(url) {
-        return await this.axios.delete(url);
-    }
-
-    async patch(url, data) {
-        return await this.axios.patch(url, data);
-    }
+  async patch(url, data) {
+    return await this.axios.patch(url, data);
+  }
 }
 
-export const api = new API()
+export const api = new API();
 
 export default {
-    install(app) {
-        app.config.globalProperties.$api = api;
-    }
-}
+  install(app) {
+    app.config.globalProperties.$api = api;
+  },
+};
